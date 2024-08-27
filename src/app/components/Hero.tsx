307012fada883c7e1c89ce90a255439e2c1fbae2
mac <mac@macs-MacBook-Pro-2.local>
2024-08-27 22:45:43 +0400

    first commit

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,54 +1,55 @@
-"use client"
+"use client";
 
-import Slider from "react-slick"
-import { Slide } from "./Slide"
+import Slider from "react-slick";
+import { Slide } from "./Slide";
 
 export const Hero = () => {
-  var settings={
+  var settings = {
     dots: true,
     Infinity: true,
     slidesToShow: 1,
     slidesToScrol: 1,
     autoplay: true,
     pauseOnHover: false,
-  }
-  const slideData=[
-  {
-    id: 0,
-    img: "/banner-1 (1).webp",
-    title: "Trending Item",
-    mainTitle: "WOMEN'S LATEST FASHION SALE",
-    price: "20$"
-  },
-  {
-    id: 1,
-    img: "/banner-2.webp",
-    title: "Trending Accessories",
-    mainTitle: "MODELE SUNGLASSES",
-    price: "25$"
-  },
-  {
-    id: 2,
-    img: "/banner-3.webp",
-    title: "Sale Offer",
-    mainTitle: "NEW FASHION SUMMER SALE",
-    price: "37$"
-  }]
+  };
+  const slideData = [
+    {
+      id: 0,
+      img: "/banner-1 (1).webp",
+      title: "Trending Item",
+      mainTitle: "WOMEN'S LATEST FASHION SALE",
+      price: "20$",
+    },
+    {
+      id: 1,
+      img: "/banner-2.webp",
+      title: "Trending Accessories",
+      mainTitle: "MODELE SUNGLASSES",
+      price: "25$",
+    },
+    {
+      id: 2,
+      img: "/banner-3.webp",
+      title: "Sale Offer",
+      mainTitle: "NEW FASHION SUMMER SALE",
+      price: "37$",
+    },
+  ];
   return (
     <div>
       <div className="container pt-6 lg:pt-0">
         <Slider {...settings}>
           {slideData.map((item) => (
-          <Slide
-          key={item.id}
-          img={item.img}
-          title={item.title}
-          mainTitle={item.mainTitle}
-          price={item.price}
-          />
+            <Slide
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              mainTitle={item.mainTitle}
+              price={item.price}
+            />
           ))}
         </Slider>
       </div>
     </div>
-  )
-}
+  );
+};
